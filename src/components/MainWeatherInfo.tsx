60cf12179e6d213b44594ec5c03b3af27beb5f83
Rostyslav <hasiukrostyslav@gmail.com>
2023-10-15 21:40:20 +0300

    added night weather icon functionality

--- src/components/MainWeatherInfo.tsx
+++ src/components/MainWeatherInfo.tsx
@@ -3,9 +3,26 @@ import { WEATHER_TYPES } from '../data';
 import WeatherIcon from './WeatherIcon';
 
 function MainWeatherInfo() {
-  const { locationName, temperature, weatherCode } = useTypedSelector(
+  const { locationName, temperature, weatherCode, isDay } = useTypedSelector(
     (state) => state.weather
   );
+
+  function selectDayType(isDay: boolean, weatherCode: number) {
+    if (weatherCode === 0 && !isDay) return WEATHER_TYPES[100];
+    else return WEATHER_TYPES[weatherCode as keyof typeof WEATHER_TYPES];
+  }
+
+  function selectDayIcon(isDay: boolean, weatherCode: number) {
+    if (weatherCode === 0 && !isDay) return <WeatherIcon code={100} />;
+    if ((weatherCode === 1 || weatherCode === 2 || weatherCode === 3) && !isDay)
+      return <WeatherIcon code={101} />;
+    if (
+      (weatherCode === 61 || weatherCode === 63 || weatherCode === 65) &&
+      !isDay
+    )
+      return <WeatherIcon code={102} />;
+    else return <WeatherIcon code={weatherCode} />;
+  }
 
   return (
     <div className="md:mr-20">
@@ -14,7 +31,9 @@ function MainWeatherInfo() {
       </p>
       <div className="flex items-center mt-4 mb-3 gap-7">
         <div className="text-7xl text-sky-500">
-          <WeatherIcon code={weatherCode} />
+          {isDay !== null &&
+            weatherCode !== null &&
+            selectDayIcon(isDay, weatherCode)}
         </div>
         <p className="text-9xl font-thin text-sky-600 dark:text-sky-400">
           {temperature}
@@ -22,7 +41,9 @@ function MainWeatherInfo() {
         </p>
       </div>
       <p className="text-xl text-sky-700 dark:text-sky-500 font-bold">
-        {WEATHER_TYPES[weatherCode as keyof typeof WEATHER_TYPES]}
+        {isDay !== null &&
+          weatherCode !== null &&
+          selectDayType(isDay, weatherCode)}
       </p>
     </div>
   );
