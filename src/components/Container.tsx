de924a3ab3b39796508bfd7b6c47c2075a4aa138
Rostyslav <hasiukrostyslav@gmail.com>
2023-10-15 23:50:47 +0300

    fixed responsible input

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 
 function Container({ children }: ContainerProps) {
   return (
-    <div className="flex flex-col justify-center mx-auto w-max py-6 relative min-h-screen md:min-w-[700px]">
+    <div className="flex flex-col justify-center mx-auto w-9/12 sm:w-[500px] py-6 relative min-h-screen md:min-w-[700px]">
       {children}
     </div>
   );
